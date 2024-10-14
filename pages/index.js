da11a351064aafc0fef8befdb6653df826c39841
Tom <tomek286@yahoo.com>
2024-10-14 13:27:15 +0200

    seo

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import Footer from "@/components/Footer";
 import ButtonsGroup from "@/components/ButtonsGroup";
 import styled from "styled-components";
+import Head from "next/head";
 
 const BackgroundContainer = styled.div`
    background-image: url('/bg-engine.avif');
@@ -54,8 +55,31 @@ export default function HomePage({featuredProduct, newProducts}) {
 
   return (
     <div>
-            {/* <BackgroundImage />
-            <Overlay /> */}
+              <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Kvalitní autodíly pro Mercedes – Rychlé a spolehlivé dodání | Auto Dily Express</title>
+        <meta
+          name="description"
+          content="Objevte širokou nabídku kvalitních autodílů pro vozy Mercedes. Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka, skvělé ceny a spolehlivý servis. Vaše auto si zaslouží to nejlepší!"
+        />
+
+        <link rel="cannonical" href="https://autodilyexpress.cz" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/auto-dily-logo.png" />
+        <meta property="og:locale" content="cs_CZ" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
+        <meta property="og:image" content="/auto-dily-logo.png" />
+        <meta property="og:image:width" content="50" />
+        <meta property="og:image:height" content="50" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:description" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
+        <meta property="og:url" content="https://autodilyexpress.cz" />
+        <meta property="og:site_name" content="autodilyexpress.cz" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka" />
+        <meta name="twitter:description" content="Nabízíme motorové díly, prvky karoserie, elektrické součástky a další. Rychlá dodávka"></meta>
+
+      </Head>
       <Header setMenuOpen={setMenuOpen} /> 
       <ButtonsGroup />
       <Featured product={featuredProduct} menuOpen={menuOpen} /> 
