31fbcf0904f921a9fa852364273d2f79f249dd2b
Tom <tomek286@yahoo.com>
2024-10-20 11:26:26 +0200

    headerbg

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,14 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Center from "@/components/Center";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CartContext } from "@/components/CartContext";
 import BarsIcon from "@/components/icons/Bars";
-import Image from "next/image";
 import Spinner from "@/components/Spinner"; 
 import { useRouter } from "next/router"; 
 
-// const StyledHeader = styled.header`
-//   background-color: #dcdcdc;
-//   z-index: 4; 
-//   position: relative;
-//   border-radius: 20px;
-//   margin-bottom: 12px;
-// `;
-
 const StyledHeader = styled.header`
-  background-color: #dcdcdc;
+  background-color: rgba(220, 220, 220, ${(props) => props.opacity});
   z-index: 10;
   position: fixed;
   top: 12px;
@@ -28,9 +19,8 @@ const StyledHeader = styled.header`
   border-radius: 20px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5); 
   box-sizing: border-box;
+  transition: background-color 0.3s ease-in-out; 
 `;
-
-
 
 const Logo = styled(Link)`
   color: black;
@@ -41,29 +31,23 @@ const Logo = styled(Link)`
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  
+  justify-content: space-between;  
   padding: 20px 0;
 `;
 
 const StyledNav = styled.nav`
-  ${props => props.mobileNavActive ? `
-    display: block;
-  ` : `
-    display: none;
-  `}
+  ${props => props.mobileNavActive ? `display: block;` : `display: none;`}
   gap: 25px;
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-  
   padding: 70px 20px 20px;
   background-color: #222;
   z-index: 4; 
   @media screen and (min-width: 768px) {
-    background-color: #dcdcdc;
+    background-color: transparent; 
     display: flex;
     position: static;
     padding: 0;
@@ -79,6 +63,9 @@ const NavLink = styled.a`
   @media screen and (min-width: 768px) {
     padding: 0;
     color: black;
+  }
+  &:hover {
+    background: none; 
   }
 `;
 
@@ -103,7 +90,8 @@ const MainContent = styled.div`
 export default function Header({ setMenuOpen }) {
   const { cartProducts } = useContext(CartContext);
   const [mobileNavActive, setMobileNavActive] = useState(false);
-  const [isLoading, setIsLoading] = useState(false); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [headerOpacity, setHeaderOpacity] = useState(1); 
   const router = useRouter();
 
   const toggleMenu = () => {
@@ -116,26 +104,40 @@ export default function Header({ setMenuOpen }) {
     router.push(url).then(() => setIsLoading(false)); 
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY; 
+      const newOpacity = Math.max(1 - scrollTop / 200, 0.5); 
+      setHeaderOpacity(newOpacity);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-    <StyledHeader>
-      <Center>
-        <Wrapper>
-          <Logo href={'/'}>Auto Dily</Logo>
-          <StyledNav mobileNavActive={mobileNavActive}>
-            <NavLink onClick={() => handleNavigation('/')}>Home</NavLink>
-            <NavLink onClick={() => handleNavigation('/products')}>Všechny produkty</NavLink>
-            <NavLink onClick={() => handleNavigation('/podpora')}>Podpora</NavLink>
-            <NavLink onClick={() => handleNavigation('/cart')}>Košík ({cartProducts.length})</NavLink>
-          </StyledNav>
-          <NavButton onClick={toggleMenu}>
-            <BarsIcon />
-          </NavButton>
-        </Wrapper>
-        {isLoading && <Spinner />} 
-      </Center>
-    </StyledHeader>
-    <MainContent />
+      <StyledHeader opacity={headerOpacity}>
+        <Center>
+          <Wrapper>
+            <Logo href={'/'}>Auto Dily</Logo>
+            <StyledNav mobileNavActive={mobileNavActive}>
+              <NavLink onClick={() => handleNavigation('/')}>Home</NavLink>
+              <NavLink onClick={() => handleNavigation('/products')}>Všechny produkty</NavLink>
+              <NavLink onClick={() => handleNavigation('/podpora')}>Podpora</NavLink>
+              <NavLink onClick={() => handleNavigation('/cart')}>Košík ({cartProducts.length})</NavLink>
+            </StyledNav>
+            <NavButton onClick={toggleMenu}>
+              <BarsIcon />
+            </NavButton>
+          </Wrapper>
+          {isLoading && <Spinner />} 
+        </Center>
+      </StyledHeader>
+      <MainContent />
     </div>
   );
 }
